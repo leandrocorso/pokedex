f10386551404d0a017797aa76087e27469b4f0a4
Leandro Corso <contato@leandrocorso.com.br>
2020-06-13 17:13:26 -0300

    redux persisit data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,7 +8,7 @@ import {
 } from 'react-router-dom';
 
 // Store
-import store from 'store';
+import { store, persistor } from 'store';
 
 // Containers
 import Home from 'containers/Home';
@@ -22,14 +23,16 @@ import GlobalStyle from 'theme/GlobalStyle';
 const App = () => (
   <Router>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/:name" component={Details} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-        <GlobalStyle />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/:name" component={Details} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+          <GlobalStyle />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </Router>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducers from './rootReducers';
 import rootSagas from './rootSagas';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducers, applyMiddleware(sagaMiddleware));
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSagas);
 
-export default store;
+export { store, persistor };
